f826d977a4fe1df63acf1d4cc51f0d6765c4dc77
Roberto Brambila <robertobrambila@gmail.com>
2020-12-17 09:33:01 -0800

    added input focus and validation

--- src/ui/scripts/scripts.js
+++ src/ui/scripts/scripts.js
@@ -1,16 +1,28 @@
 
+// set focus on divisions input field
+const textbox = document.getElementById('divisionCount');
+textbox.focus();
+textbox.select();
+
+// simulate button click when you hit Enter in the input field
+textbox.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById('ok').click();
+    }
+  });
+
 // button click
 document.getElementById('ok').onclick = () => {
-    const textbox = document.getElementById('divisionCount');
     const divisionCount = parseInt(textbox.value, 10);
-    if (divisionCount <= 0) {
-        textbox.style.borderColor = "#f24822"; // red from figma-plugin-ds css
+    if (divisionCount <= 0 || textbox.value.length == 0) {
+        textbox.style.borderColor = '#f24822'; // red from figma-plugin-ds css
         //set message
         document.getElementById('notification').innerHTML = '<div class="icon icon--warning icon--red"></div><div class="type">Value must be higher than 0.</div>';
         // clear
         setTimeout(function () {
-            document.getElementById("notification").innerHTML = "&nbsp;";
-            textbox.style.borderColor = "white";
+            document.getElementById('notification').innerHTML = '&nbsp;';
+            textbox.style.borderColor = 'white';
         }, 3500);
     }
     else {
@@ -28,7 +40,7 @@ onmessage = (event) => {
         document.getElementById('notification').innerHTML = '<div class="icon icon--warning icon--red"></div><div class="type">No valid path(s) selected.</div>';
         // clear
         setTimeout(function () {
-            document.getElementById("notification").innerHTML = "&nbsp;";
+            document.getElementById('notification').innerHTML = '&nbsp;';
         }, 3500);
     }
 }
